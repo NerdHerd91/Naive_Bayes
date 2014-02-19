524524bbbe4cbf0810072d7409baeff106d1b646
NerdHerd91 <sventrel@outlook.com>
2014-02-19 15:15:44 -0800

    Changed parsing style

--- Bayes.java
+++ Bayes.java
@@ -8,9 +8,18 @@ import java.util.HashSet;
 public class Bayes {
 
 	public static void main(String[] args) {
+		// Parse the smoothing parameter to use.
+		int smooth = 1;
+		try {
+			smooth = Integer.parseInt(args[0]);
+		} catch (Exception e) {
+			System.out.println("Please be sure to include a valid smoothing parameter.");
+			System.exit(0);
+		}
+		
 		// Sets that store a list of emails
-		Set<Email> trainEmails = new HashSet<Email>();
-		Set<Email> testEmails = new HashSet<Email>();
+		Map<String, Map<String, Integer>> trainEmails = new HashMap<String, Map<String, Integer>>();
+		Map<String, Map<String, Integer>> testEmails = new HashMap<String, Map<String, Integer>>();
 
 		// Parse training and test data into sets.
 		parseEmails(trainEmails, "./DataSet/train");
@@ -23,25 +32,32 @@ public class Bayes {
 	* @param emails Reference to the set to place emails we create into.
 	* @param fileName File path to the file containing the emails to parse.
 	*/
-	public static void parseEmails(Set<Email> emails, String fileName) {
+	public static void parseEmails(Map<String, Map<String, Integer>> emails, String fileName) {
 		try {
+			// Initialize the spam/ham word count maps.
+			emails.put("spam", new HashMap<String, Integer>());
+			emails.put("ham", new HashMap<String, Integer>());
+
 			Scanner sc = new Scanner(new File(fileName));
 			while (sc.hasNextLine()) {
-				String[] tokens = sc.nextLine().split(" ");
-				String eid = tokens[0];
-				boolean spam = (tokens[1].equals("spam")) ? true : false;
-				Email email = new Email(eid, spam, new HashMap<String, Integer>());
+				Scanner line = new Scanner(sc.nextLine());
+				line.next();
+				String label = line.next();
 
-				for (int i = 2; i < tokens.length; i+=2) {
-					String word = tokens[i];
-					int count = Integer.parseInt(tokens[i+1]);
-					email.getWords().put(word, count);
+				while (line.hasNext()) {
+					String word = line.next();
+					int count = line.nextInt();
+
+					if (emails.get(label).containsKey(word)) {
+						int prev = emails.get(label).get(word);
+						emails.get(label).put(word, prev +count);
+					} else {
+						emails.get(label).put(word, count);
+					}
 				}
-				emails.add(email);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 	}
 }
